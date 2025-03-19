748a60a30244e25be9cac6d0bb734cab1ca78e3a
Manuel Mango <manuelmangoinfo@gmail.com>
2025-03-19 12:35:14 -0300

    7c

--- P1/questions.py
+++ P1/questions.py
@@ -30,13 +30,17 @@ posible_answers = [0,1,2,3]
 players_score = 0
 #TRAIGO LOS NUMEROS
 numbers = string.digits
+#A continuacion, creo la variable questions_to_ask, lo que sera una tupla con pregunta, respuesta e indice de respuesta correcta.
+questions_data = list(zip(questions,answers,correct_answers_index))
+#Selecciono 3 tuplas aleatorias
+questions_to_ask = random.choices(questions_data, k = 3)
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+for question, answers, correct_answers_index in questions_to_ask:
+    print(question) #imprimo la pregunta aleatoria
+    #question_index = random.randint(0, len(questions) - 1) : deshice esta linea ya que la pregunta random la seleccionamos con random.choices
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    #print(questions[question_index]) #deshice esta linea, ya que la seleccion aleatoria la hicimos.
+    for i, answer in enumerate(answers):
         print(f"{i + 1}. {answer}")
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -44,10 +48,13 @@ for _ in range(3):
         # Se verifica si la respuesta es correcta
         if user_answer in numbers:
             user_answer = int(user_answer) - 1
-            if user_answer == correct_answers_index[question_index]:
+            if user_answer == correct_answers_index:
                 print("¡Correcto!")
                 players_score = players_score + 1
                 break
+            elif user_answer not in range(4):
+                print('Numero no valido')
+                sys.exit(1)
             else:
                 players_score = players_score - 0.5
         else:
@@ -57,7 +64,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(answers[correct_answers_index])
     # Se imprime un blanco al final de la pregunta
     print()
 print(f'el score conseguido por el participante es de {players_score}')
