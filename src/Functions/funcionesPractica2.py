29c35eacde6d895dccdcb1783b882e764ccc46af
Manuel Mango <manuelmangoinfo@gmail.com>
2025-03-27 12:00:07 -0300

    Subida ej3 y 4

--- src/Functions/funcionesPractica2.py
+++ src/Functions/funcionesPractica2.py
@@ -1,3 +1,5 @@
+import sys
+
 def chequearZen(zen_text, vocales):
     for linea in zen_text:
         palabras = linea.split()[1]
@@ -11,5 +13,27 @@ def maximoTitle(titles, longitud_titles):
         if(len(titles[i]) > max_len):
             max_len = len(titles[i])
             max_title = titles[i]
-            
+
     return max_title
+
+def registrarse(mayus,numeros,validos):
+    sumaMayus = 0
+    sumaNum = 0
+    user_password = input('Ingrese su nombre de usuario: ')
+    if(len(user_password) > 4):
+        for letra in user_password:
+            if letra in mayus:
+                sumaMayus += 1
+            if letra in numeros:
+                sumaNum += 1
+        if letra not in validos:
+            print('Caracter no valido')
+            sys.exit(1)
+        if sumaMayus > 0 and sumaNum > 0 :
+            print('Usuario registrado')
+        if sumaMayus == 0:
+            print('Falta mayuscula')
+        if sumaNum == 0:
+            print('Falta numero')
+    else:
+        print('El nombre debe contener 5 caracteres como minimo, y sin caracteres especiales (debe contener una mayuscula y un numero)')
