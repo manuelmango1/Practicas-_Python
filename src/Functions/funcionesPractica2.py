ad0f75df197290bd829c68c1368f408bb99b77ae
Manuel Mango <manuelmangoinfo@gmail.com>
2025-03-31 16:11:10 -0300

    EJ7

--- src/Functions/funcionesPractica2.py
+++ src/Functions/funcionesPractica2.py
@@ -1,12 +1,14 @@
 import sys
-
+import string
+import random
+#EJERCICIO 1 FUNCIONES
 def chequearZen(zen_text, vocales):
     for linea in zen_text:
         palabras = linea.split()[1]
         if(len(palabras) > 1):
             if palabras.startswith(vocales):
                 print(linea)
-
+#EJERCICIO 2 FUNCIONES
 def maximoTitle(titles, longitud_titles):
     max_len = 0
     for i in range(longitud_titles):
@@ -15,7 +17,7 @@ def maximoTitle(titles, longitud_titles):
             max_title = titles[i]
 
     return max_title
-
+#EJERCICIO 4 FUNCIONES
 def registrarse(mayus,numeros,validos):
     sumaMayus = 0
     sumaNum = 0
@@ -37,3 +39,13 @@ def registrarse(mayus,numeros,validos):
             print('Falta numero')
     else:
         print('El nombre debe contener 5 caracteres como minimo, y sin caracteres especiales (debe contener una mayuscula y un numero)')
+
+#EJERCICIO 7 FUNCIONES
+def generarClave(usuario, fecha_actual):
+    letras = string.ascii_letters
+    numeros = string.digits
+    clave_base = usuario + "-" + fecha_actual + "-"
+    while (len(clave_base) < 30):
+        randomClave = random.choice(letras + numeros)
+        clave_base += randomClave
+    return clave_base
